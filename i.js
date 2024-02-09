56e62dd6f7e21430626c1fabe4eab866016b96eb
juanvelasco1 <131167099+juanvelasco1@users.noreply.github.com>
2024-02-09 17:50:47 -0500

    2

---
+++ i.js
@@ -0,0 +1,24 @@
+const studentGrades = [100, 50, 65, 70, 85, 96, 45];
+const result = computeGrades(studentGrades);
+console.log(result);
+
+function studentGradess(result) {
+	let studentGrades = 0;
+	if (60) {
+		Aprove;
+	} else {
+		Aprove;
+	}
+  if () {
+
+  } else {
+
+  }
+	persons.forEach((result) => {
+		studentGrades > 60;
+	});
+	return studentGrades;
+}
+
+const notas = studentGrades(result);
+console.log('Las notas aprovadas son:', notas);
